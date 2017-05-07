814410fd5feac4377e871dd91866d9615366dbcb
Arman Bilge <armanbilge@gmail.com>
2017-05-07 11:12:22 -0700

    Refactor for more flexible DP

--- src/main/scala/prelim/DirichletProcess.scala
+++ src/main/scala/prelim/DirichletProcess.scala
@@ -5,17 +5,19 @@ import monocle.Lens
 import org.apache.commons.math3.special.Gamma
 import prelim.DirichletProcess.IsCluster
 
-class DirichletProcess[A <: Double, H <: Probability[Double], X](val xs: IndexedSeq[X], val alpha: A, val clusters: Set[H])(_Z: => Double = clusters.size * math.log(alpha) - xs.indices.map(i => math.log(alpha + i)).sum)(implicit val cl: IsCluster[H, X]) extends Probability[Double] {
+class DirichletProcess[A <: Double, H <: Probability[Double], X](_xs: => IndexedSeq[X], val alpha: A, val clusters: Set[H])(implicit val cl: IsCluster[H, X]) extends Probability[Double] {
 
-  lazy val Z: Double = _Z
+  lazy val xs: IndexedSeq[X] = _xs
 
-  override lazy val evaluate: Double = Z + clusters.view.map(x => Gamma.logGamma(cl.size(x))).sum + clusters.view.map(_.evaluate).sum
+  lazy val Z: Double = clusters.size * math.log(alpha) - xs.indices.map(i => math.log(alpha + i)).sum
+
+  override lazy val evaluate: Double =  Z + clusters.view.map(x => Gamma.logGamma(cl.size(x))).sum + clusters.view.map(_.evaluate).sum
 
 }
 
 object DirichletProcess {
 
-  def apply[A <: Double, H <: Probability[Double], X](xs: IndexedSeq[X], alpha: A)(implicit cl: IsCluster[H, X]) = new DirichletProcess[A, H, X](xs, alpha, Set(cl.of(xs.toSet)))()
+  def apply[A <: Double, H <: Probability[Double], X](xs: IndexedSeq[X], alpha: A)(implicit cl: IsCluster[H, X]) = new DirichletProcess[A, H, X](xs, alpha, xs.map(cl.of).toSet)()
 
   trait IsCluster[H, X] {
     def empty: H
@@ -29,9 +31,9 @@ object DirichletProcess {
   }
 
   implicit def _a[A <: Double, H <: Probability[Double], X]: Lens[DirichletProcess[A, H, X], A]
-    = Lens[DirichletProcess[A, H, X], A](_.alpha)(alpha => dp => new DirichletProcess[A, H, X](dp.xs, alpha, dp.clusters)(dp.Z)(dp.cl))
+    = Lens[DirichletProcess[A, H, X], A](_.alpha)(alpha => dp => new DirichletProcess[A, H, X](dp.xs, alpha, dp.clusters)(dp.cl))
 
   implicit def _clusters[A <: Double, H <: Probability[Double], X]: Lens[DirichletProcess[A, H, X], Set[H]]
-    = Lens[DirichletProcess[A, H, X], Set[H]](_.clusters)(clusters => dp => new DirichletProcess[A, H, X](dp.xs, dp.alpha, clusters)()(dp.cl))
+    = Lens[DirichletProcess[A, H, X], Set[H]](_.clusters)(clusters => dp => new DirichletProcess[A, H, X](clusters.view.flatMap(dp.cl.values).toIndexedSeq, dp.alpha, clusters)(dp.cl))
 
 }
