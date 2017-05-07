18b5ada8a4668d77a54095ef4ccd7065e81d2f8d
Arman Bilge <armanbilge@gmail.com>
2017-05-07 11:07:32 -0700

    Minor refactoring

--- src/main/scala/prelim/DPGibbsSampler.scala
+++ src/main/scala/prelim/DPGibbsSampler.scala
@@ -9,9 +9,10 @@ class DPGibbsSampler[A <: Double, H <: Probability[Double], X](implicit rng: Gen
   override def apply(dp: DirichletProcess[A, H, X]): DirichletProcess[A, H, X] = dp.xs.foldLeft(dp) { (dp, x) =>
     DirichletProcess._clusters[A, H, X].modify { clusters =>
       val cp = clusters.view.map(dp.cl.remove(_, x)).filter(dp.cl.size(_) > 0)
-      val ps = cp.map(h => (h, dp.cl.size(h) + dp.cl.posteriorPredictive(h, x))).toMap + (dp.cl.empty -> (dp.alpha + dp.cl.posteriorPredictive(dp.cl.empty, x)))
-      val scalingFactor = ps.values.max
-      val hp = rng.next[H](Multinomial[H, Double](ps.mapValues(x => math.exp(x - scalingFactor))))
+      val logPs = cp.map(h => (h, math.log(dp.cl.size(h)) + dp.cl.posteriorPredictive(h, x))).toMap + (dp.cl.empty -> (math.log(dp.alpha) + dp.cl.posteriorPredictive(dp.cl.empty, x)))
+      val scalingFactor = logPs.values.max
+      val ps = logPs.mapValues(x => math.exp(x - scalingFactor))
+      val hp = rng.next[H](Multinomial[H, Double](ps))
       cp.toSet - hp + dp.cl.add(hp, x)
     }(dp)
   }
