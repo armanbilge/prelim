9d8a2e44f2d5f28dcfe49a0c76167cc54173e73d
Arman Bilge <armanbilge@gmail.com>
2017-05-30 09:52:19 -0700

    Generalize to allow arbitrary variance for prior

--- src/main/scala/prelim/gaussian/GaussianCluster.scala
+++ src/main/scala/prelim/gaussian/GaussianCluster.scala
@@ -4,18 +4,18 @@ import mcmc.Probability
 import org.apache.commons.math3.distribution.NormalDistribution
 import prelim.DirichletProcess.IsCluster
 
-class GaussianCluster(val xs: Set[Double]) extends Probability[Double] {
+class GaussianCluster(val xs: Set[Double], val sigmasq: Double) extends Probability[Double] {
 
   val n = xs.size
 
   override def evaluate: Double = {
     val xsum = xs.sum
-    - n / 2.0 * math.log(2 * math.Pi) - math.log(n + 1) / 2 - xs.view.map(x => x * x).sum / 2 + xsum * xsum / (2 * (n + 1))
+    - n / 2.0 * math.log(2 * math.Pi) - math.log(n * sigmasq + 1) / 2 - xs.view.map(x => x * x).sum / 2 + xsum * xsum * sigmasq / (2 * (n * sigmasq + 1))
   }
 
   def predict(x: Double): Double = {
     val xsum = xs.sum
-    val `sigma_n^2` = 1.0 / (n + 1)
+    val `sigma_n^2` = 1.0 / (n + 1 / sigmasq)
     val `mu_n` = `sigma_n^2` * xsum
     new NormalDistribution(`mu_n`, math.sqrt(`sigma_n^2` + 1)).logDensity(x)
   }
@@ -31,22 +31,22 @@ class GaussianCluster(val xs: Set[Double]) extends Probability[Double] {
 
 object GaussianCluster {
 
-  implicit object GaussianIsCluster extends IsCluster[GaussianCluster, Double] {
+  def gaussianIsCluster(sigmasq: Double) = new IsCluster[GaussianCluster, Double] {
 
-    override val empty: GaussianCluster = new GaussianCluster(Set())
+    override val empty: GaussianCluster = new GaussianCluster(Set(), sigmasq)
 
     override def size(h: GaussianCluster): Int = h.n
 
-    override def of(x: Double): GaussianCluster = new GaussianCluster(Set(x))
+    override def of(x: Double): GaussianCluster = new GaussianCluster(Set(x), sigmasq)
 
-    override def of(xs: Set[Double]): GaussianCluster = new GaussianCluster(xs)
+    override def of(xs: Set[Double]): GaussianCluster = new GaussianCluster(xs, sigmasq)
 
     override def values(h: GaussianCluster): Set[Double] = h.xs
 
-    override def add(h: GaussianCluster, x: Double): GaussianCluster = new GaussianCluster(h.xs + x)
+    override def add(h: GaussianCluster, x: Double): GaussianCluster = new GaussianCluster(h.xs + x, sigmasq)
 
     override def remove(h: GaussianCluster, x: Double): GaussianCluster = if (h.xs.contains(x))
-      new GaussianCluster(h.xs - x)
+      new GaussianCluster(h.xs - x, sigmasq)
     else
       h
 
